634bbe0bc81a3a6ec478fe8faef5c47dce713073
munhunger <marcusmunger@gmail.com>
2020-09-02 20:23:39 +0200

    remove relations

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,26 @@ impl Node {
         }
     }
 
+    fn remove_relation(&mut self, child_id: u8) {
+        for (x, child) in self.relation.iter().enumerate() {
+            match child {
+                Node {
+                    data: _,
+                    id: i,
+                    relation: _,
+                } if *i == child_id => {
+                    println!(
+                        "removing node {}:{} at position {}",
+                        child.data, child_id, x
+                    );
+                    self.relation.remove(x);
+                    return;
+                }
+                _ => (),
+            }
+        }
+    }
+
     fn add_relation(&mut self, child: Node) {
         self.relation.push(child);
     }
@@ -24,6 +44,9 @@ fn main() {
     let mut node = Node::new(8, String::from("parent"));
     let child = Node::new(2, String::from("child"));
     node.add_relation(child);
-    println!("Hello, world!");
+    let child2 = Node::new(23, String::from("sibling"));
+    node.add_relation(child2);
+    println!("{},{} -> {}", node.data, node.id, node.relation[0].data);
+    node.remove_relation(2);
     println!("{},{} -> {}", node.data, node.id, node.relation[0].data);
 }
